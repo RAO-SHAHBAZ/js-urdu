ad2701c531f8c80bae17055d3a046fecacbb63a2
Rao Shahbaz <menlenon59@gmail.com>
2025-01-25 02:41:15 +0000

    Veriables and dataTypes

---
+++ 01_Basics/01_veriables.js
@@ -0,0 +1,19 @@
+const account_Id = 144
+let email = "Rao@email.com"
+var Pass = "123455"
+accountCity = "Multan"   // We CAn USe veriable without telling js to var or const by defaullt js will create a var but this is not reocmended
+
+// account_Id = 145   This Is Not Allowd in JS 
+// email = "miniamar@gmail.com"  This Is Allowd In Js , You can Change Your Veribales 
+Pass = "12343433333"
+accountCity = "Karachi"
+email = 'raoshahbaz@gmail.com'
+// console.log(email);
+ 
+
+console.table([Pass ,email , accountCity])
+
+
+/*
+Prefer Not To use Var Because Of issue in Blok scope and functionl scope
+*/
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" // trear all Js Code as newr version
+
+//   alert(3+3) we cannot use this in node js , can use in browser
+
+// Code Readabilty Should be High
+
+const king = "Rao Shahbaz"
+
+let passingMarks = undefined
+
+let metter = null
+
+console.log(typeof passingMarks);
+
+console.log(typeof metter);
+
+
